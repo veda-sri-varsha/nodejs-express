5bbd9ef48f4dd6d45e40a9417e51a45b4426ee36
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-08-06 17:11:32 +0000

    Refactor app.js to implement various routes and CRUD operations

--- src/app.js
+++ src/app.js
@@ -1,10 +1,50 @@
 const express = require('express');
 const app = express();
 
-app.use((req,res) => {
-    res.send("Hello From Server!");
-})
+app.use(express.json()); // required for POST body parsing
 
-app.listen(7000,()=>{
-    console.log("Server is running on port 7000");
-})
+// HTML homepage
+app.get('/', (req, res) => {
+  res.set('Content-Type', 'text/html');
+  res.send('<h1>Programming World!</h1>');
+});
+
+// Query parameters: /userQuery?userId=707&name=you
+app.get("/userQuery", (req, res) => {
+  const { userId, name } = req.query;
+  res.send({ userId, name });
+});
+
+// Dynamic parameters: /user/sri/apple
+app.get("/user/:firstName/:lastName", (req, res) => {
+  const { firstName, lastName } = req.params;
+  res.send({ firstName, lastName });
+});
+
+// Basic CRUD
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Apple", lastName: "Ball" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data successfully saved to the database!");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfully!");
+});
+
+// Match all methods
+app.use("/test", (req, res) => {
+  res.send("Hello from the server!");
+});
+
+// Regex route
+app.get(/.*fly$/, (req, res) => {
+  res.send("You hit a route ending with 'fly'!");
+});
+
+// Start server
+app.listen(7000, () => {
+  console.log("Server is running on port 7000");
+});
